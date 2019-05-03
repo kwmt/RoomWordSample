8c3381aded6528cbb9eda0fb50f4689347531945
Yasutaka Kawamoto <kwmt27@gmail.com>
2019-05-04 08:34:40 +0900

    primary keyとtableのカラム名を指定

--- app/src/main/java/net/kwmt27/roomwordsample/infrastructure/WordEntity.kt
+++ app/src/main/java/net/kwmt27/roomwordsample/infrastructure/WordEntity.kt
@@ -1,3 +1,8 @@
 package net.kwmt27.roomwordsample.infrastructure
 
-data class WordEntity(val word: String)
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "word_table")
+data class WordEntity(@PrimaryKey @ColumnInfo(name = "word") val word: String)
