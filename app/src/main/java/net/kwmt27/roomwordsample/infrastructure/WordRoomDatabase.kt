30b437f8b9d34c054150f74831ed0d7d727c2379
Yasutaka Kawamoto <kwmt27@gmail.com>
2019-05-04 08:57:36 +0900

    room databaseを作成

---
+++ app/src/main/java/net/kwmt27/roomwordsample/infrastructure/WordRoomDatabase.kt
@@ -0,0 +1,32 @@
+package net.kwmt27.roomwordsample.infrastructure
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [WordEntity::class], version = 1)
+abstract class WordRoomDatabase : RoomDatabase() {
+    abstract fun wordDao(): WordDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: WordRoomDatabase? = null
+
+        fun getDatabase(context: Context): WordRoomDatabase {
+            val tempInstance = INSTANCE
+            if (tempInstance != null) {
+                return tempInstance
+            }
+            synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    WordRoomDatabase::class.java,
+                    "Word_database"
+                ).build()
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
+}
