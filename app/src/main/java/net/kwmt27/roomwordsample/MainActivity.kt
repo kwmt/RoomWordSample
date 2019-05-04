243cb03769c8562d9445b65dbce1a4e640ca91a6
Yasutaka Kawamoto <kwmt27@gmail.com>
2019-05-04 09:46:28 +0900

    Add a RecyclerView

--- app/src/main/java/net/kwmt27/roomwordsample/MainActivity.kt
+++ app/src/main/java/net/kwmt27/roomwordsample/MainActivity.kt
@@ -1,12 +1,16 @@
 package net.kwmt27.roomwordsample
 
+import android.content.Context
 import android.os.Bundle
+import android.view.*
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity;
-import android.view.Menu
-import android.view.MenuItem
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 import kotlinx.android.synthetic.main.activity_main.*
+import net.kwmt27.roomwordsample.infrastructure.WordEntity
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,6 +23,11 @@ class MainActivity : AppCompatActivity() {
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerview)
+        val adapter = WordListAdapter(this)
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -35,5 +44,34 @@ class MainActivity : AppCompatActivity() {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    class WordListAdapter internal constructor(
+        context: Context
+    ) : RecyclerView.Adapter<WordListAdapter.WordViewHolder>() {
+
+        private val inflater: LayoutInflater = LayoutInflater.from(context)
+        private var words = emptyList<WordEntity>() // Cached copy of words
+
+        inner class WordViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+            val wordItemView: TextView = itemView.findViewById(R.id.textView)
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WordViewHolder {
+            val itemView = inflater.inflate(R.layout.recyclerview_item, parent, false)
+            return WordViewHolder(itemView)
+        }
+
+        override fun onBindViewHolder(holder: WordViewHolder, position: Int) {
+            val current = words[position]
+            holder.wordItemView.text = current.word
+        }
+
+        internal fun setWords(words: List<WordEntity>) {
+            this.words = words
+            notifyDataSetChanged()
+        }
+
+        override fun getItemCount() = words.size
     }
 }
